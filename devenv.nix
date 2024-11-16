6c8d25f0b27f51a3087056278313f9f52a662fb1
Drew Dalmedo <andrewdalmedo@gmail.com>
2024-11-16 12:04:29 -0500

    Add commonly used commands as scripts

--- devenv.nix
+++ devenv.nix
@@ -4,4 +4,12 @@
   packages = [ pkgs.git ];
 
   languages.rust.enable = true;
+
+  scripts = {
+    run.exec = "cargo run";
+    rr.exec = "cargo run --release"; # run release binary
+    build.exec = "cargo build";
+    release.exec = "cargo build --release";
+    clean.exec = "rm -rf ./target";
+  };
 }
